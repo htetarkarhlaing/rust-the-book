12ef061180e7346a0fd266c2da5f199106e28513
Htet Arkar Hlaing <htet.arkar.hlaing.official.mm@gmail.com>
2022-07-11 17:23:34 +0630

    👻 : fixed guessing game loop

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,35 +1,31 @@
 use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
+use colored::*;
 
 fn main() {
     println!("Guess the number!");
-
     let secret_number = rand::thread_rng().gen_range(1..101);
-
     println!("The secret number is {}", secret_number);
+    loop {
+        println!("Please input your guess.");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Please input the number.");
 
-    println!("Please input your guess: ");
-
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line.");
-
-    println!("You guessed : {}", guess);
-
-    let guess: u32 = match guess.trim().parse() {
-        Ok(num) => num,
-        Err(_) => continue,
-    };
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => {
-            println!("You win!");
-            break;
+        let guess: u32 = match guess.trim().parse() {
+                Ok(num) => num,
+                Err(_) => continue,
+            };
+        println!("You guessed {}.", guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("{}","Too small".yellow()),
+            Ordering::Greater => println!("{}","Too Big".red()),
+            Ordering::Equal => {
+                println!("{}","You win.".green());
+                break;
+            }
         }
     }
 }
